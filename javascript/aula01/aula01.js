b690e094a0bc72924c1d4906f83eef194a712d27
joao castro <joaocastro_125@hotmail.com>
2023-02-07 16:59:32 -0300

    treinamento javascript

---
+++ javascript/aula01/aula01.js
@@ -0,0 +1,38 @@
+// treinamento javascript basico
+
+/**
+ * variaveis
+ * tipos de dados
+ * operadores matematicos 
+*/
+
+let data=new Date()
+
+console.log(data) // mostra a hora atual 
+
+let nome="joaocastro"
+console.log(nome)
+let idade=34
+console.log(idade)
+let ligado=true;
+console.log(ligado)
+let listaDeLinguaguem=["php","c#","javascript","java"]
+console.log(listaDeLinguaguem)
+let pessoa={
+    nome:"joao",
+    idade:34
+}
+console.log(pessoa)
+
+
+// tipos de variaveis let -> variaveis de escopo local , var -> variaveis de escopo global, const ->  variaveis que não podem ser alterados 
+
+// operadores(+(soma),-(subtracao)*(mutiplacao),/(divisão) %(modulo))
+
+let numero1=10
+let numero2=20
+
+let resultado=numero1+numero2
+
+console.log(resultado)
+
