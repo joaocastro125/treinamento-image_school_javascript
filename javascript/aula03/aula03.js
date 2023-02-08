eda5625f77a0f87a1259c8e52c1cc950ec5f1a93
joao castro <joaocastro_125@hotmail.com>
2023-02-07 22:02:11 -0300

    atualizando o treinamento

---
+++ javascript/aula03/aula03.js
@@ -0,0 +1,24 @@
+// condicional if e else
+// let nota = 5
+
+// if(nota>6){
+//     console.log("passou de ano")
+// }else if(nota>=5 && nota<6 ){
+//     console.log("ficou de repuperação")
+// }
+// else{
+//     console.log("foi reprovado")
+// }
+
+
+let nota = 5
+const nomeVelho="fernando"
+
+if(nota>6){
+let novoNovo="joao"
+novoNovo="chico"
+
+nomeVelho=novoNovo
+
+}
+console.log(nomeVelho)
